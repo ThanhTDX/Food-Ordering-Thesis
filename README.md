21d5e54e1b9511472f691d71ed45ba37f993593d
ThanhTDX <67357716+ThanhTDX@users.noreply.github.com>
2024-03-07 11:23:44 +0700

    fix routing

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Food-Ordering-Thesis
- made with Django
+made with Django
+
+## Version
+
+- Python: 3.12.2
+- Django: 5.0.3
+
+## Create virtual environment
+
--- food-ordering/app/starting/urls.py
+++ food-ordering/app/starting/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.main, name='main'),
-    path('members/link1', views.link1, name='link1'),
-    path('members/link2', views.link2, name='link2'),
-    path('members/link3', views.link3, name='link3'),
-    path('members/link4', views.link4, name='link4'),
+    path('', views.main, name='main'),
+    path('link1', views.link1, name='link1'),
+    path('link2', views.link2, name='link2'),
+    path('link3', views.link3, name='link3'),
+    path('link4', views.link4, name='link4'),
 ]
