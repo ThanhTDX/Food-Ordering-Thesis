89bb7f7af88b5b9416d3b1d97275020de26ef33b
ThanhTDX <67357716+ThanhTDX@users.noreply.github.com>
2024-05-07 17:02:52 +0700

    finish register form

--- food-ordering/app/starting/forms.py
+++ food-ordering/app/starting/forms.py
@@ -1,14 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
 from django import forms
-
 from django.forms.widgets import PasswordInput, TextInput
 
 
 # - Create/Register a user (Model Form)
 
-class CreateUserForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
 
     class Meta:
 
@@ -19,8 +17,8 @@ class CreateUserForm(UserCreationForm):
 # - Authentication Form
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=TextInput())
-    password = forms.CharField(widget=PasswordInput())
+    username = forms.CharField(label="Username", widget=TextInput())
+    password = forms.CharField(label="Password", widget=PasswordInput())
     
     
 # Reservation Form
