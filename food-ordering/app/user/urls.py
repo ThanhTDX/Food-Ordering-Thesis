23a3747451cd0f5c3821b6ca19c6665382ca9f6d
ThanhTDX <67357716+ThanhTDX@users.noreply.github.com>
2024-06-24 15:36:30 +0700

    removed /user

--- food-ordering/app/user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('/', views.index, name='index'),
-    path('user_logout', views.user_logout, name='user_logout')
-]
--- food-ordering/app/user/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def user_logout(request):
-    auth.logout(request)
-    return request("")
