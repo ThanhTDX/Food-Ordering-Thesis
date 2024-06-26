b1b1b54d7710a0b64a61b93ebac3549792648c8e
ThanhTDX <67357716+ThanhTDX@users.noreply.github.com>
2024-06-26 18:14:53 +0700

    remove old code with local variables

--- food-ordering/app/starting/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User1, Food, Reservation
-
-admin.site.register(User1)
-admin.site.register(Food)
-admin.site.register(Reservation)
-# Register your models here.
