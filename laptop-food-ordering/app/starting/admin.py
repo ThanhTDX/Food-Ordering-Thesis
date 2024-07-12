90e58d5c56dc8d17ede49a18f5a979af18c399af
ThanhTDX <67357716+ThanhTDX@users.noreply.github.com>
2024-07-12 13:25:21 +0700

    remove unrelated project integration

--- laptop-food-ordering/app/starting/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Food, Reservation
-
-# Register your models here.
-
-admin.site.register(Food)
-admin.site.register(Reservation)
--- laptop-food-ordering/app/starting/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StartingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'starting'
