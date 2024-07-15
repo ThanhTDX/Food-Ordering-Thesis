72a550cde1bfe2e65ef06e80d7566296de88ec1f
ThanhTDX <67357716+ThanhTDX@users.noreply.github.com>
2024-07-15 10:10:01 +0700

    separate menu

--- food-ordering/app/restaurant/views.py
+++ food-ordering/app/restaurant/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
 from . models import Food
-from . forms import ReservationForm
+from . forms import ReservationForm , ReservationTableForm , ReservationVipForm
 # Create your views here.
 
 
@@ -12,22 +12,39 @@ def ReservationView(request):
   
   # POST REQUEST
   if request.method == 'POST' :
-    form = ReservationForm(request.POST)
-    if form.is_valid():
-      customer_name = form.cleaned_data('customer_name')
-      phone_num = form.cleaned_data('phone_num')
-      vip_room = form.cleaned_data('vip_room')
-      eat_time = form.cleaned_data('eat_time')
-      num_of_people = form.cleaned_data('num_of_people')
-      review = form.cleaned_data('review')
-      form.save()
+    reservation_form = ReservationForm(request.POST)
+    reservation_table = ReservationTableForm(request.POST)
+    reservation_vip = ReservationVipForm(request.POST)
+    
+    if reservation_form.is_valid() and reservation_table.is_valid() and reservation_vip.is_valid():
+      # customer_name = reservation_form.cleaned_data('customer_name')
+      # phone_num = reservation_form.cleaned_data('phone_num')
+      
+      # eat_date = reservation_form.cleaned_data('eat_date')
+      # eat_time = reservation_form.cleaned_data('eat_time')
+      # num_of_people = reservation_form.cleaned_data('num_of_people')
+      # special_message = reservation_form.cleaned_data('special_message')
+      
+      # table = reservation_table.cleaned_data('table_name')
+      # vip_room = reservation_vip.cleaned_data('vip_room')
+      
+      reservation_form.save()
+      reservation_table.save()
+      reservation_vip.save()
       return HttpResponseRedirect('/')
   
    # GET REQUEST 
   else:
-    form = ReservationForm()
+    reservation_form = ReservationForm()
+    reservation_table = ReservationTableForm()
+    reservation_vip = ReservationVipForm()
+    
   
-  context = {'reservationForm' : form }
+  context = {
+    'reservationForm' : reservation_form,
+    'reservationTable' : reservation_table,
+    'reservationVip' : reservation_vip
+  }
   return render(request, template, context)
 
 def OrderView(request):
