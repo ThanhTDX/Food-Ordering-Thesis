31b2c136717d7727e76889ceaea2b8b5855b0c9f
ThanhTDX <67357716+ThanhTDX@users.noreply.github.com>
2024-07-19 11:43:39 +0700

    forms functionality (connect to backend) finished

--- food-ordering/app/app/settings.py
+++ food-ordering/app/app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'widget_tweaks',
+    "phonenumber_field",
     
     'starting',
     'restaurant',
--- food-ordering/app/restaurant/forms.py
+++ food-ordering/app/restaurant/forms.py
@@ -3,53 +3,65 @@ from django import forms
 from django.forms import ModelForm
 from django.forms.widgets import TextInput
 from . models import Reservation , ReservationTable , ReservationVip
-from datetime import datetime
+from datetime import date
 
 # Create your Forms here
 
 # Reservation Form
 
 class ReservationForm(forms.ModelForm):
-    # customer_name = forms.CharField(label="Customer Name", max_length=100)
-    # phone_num = forms.CharField(label="Phone Number")
     
-    # eat_date = forms.DateField(
-    #     label="Eat Date",
-    #     widget=forms.DateInput(
-    #         attrs=  {
-    #             'class':'form-control col-6',
-    #             'type':'date'
-    #         }
-    #     ),
-    #     # initial='2024-5-9'
-    # )
+    MINUTE_CHOICE = (
+        (0, 0),
+        (15, 15),
+        (30, 30),
+        (45, 45),
+    )
     
-    # eat_time = forms.TimeField(
-    #     label="Eat Time",
-    #     widget=forms.TimeInput(
-    #         attrs=  {
-    #             'class':'form-control col-6',
-    #             'type':'time'
-    #         }
-    #     ),
-    #     # initial='2024-5-9'
-    # )
+    HOUR_CHOICE = ( [(x, x) for x in range(1,12)] )
     
-    # num_of_people = forms.IntegerField(label="Number of People")
-    # special_message = forms.CharField(label="Special Message")
+    MONTH_CHOICE = ( [(x, x) for x in range(1,12)] )
+    
+    DAY_CHOICE = ( [(x, x) for x in range(1,31)] )
+    
+    eat_date = forms.TypedChoiceField(
+        label="Day",
+        choices=DAY_CHOICE,
+        coerce=int,
+        empty_value=DAY_CHOICE[date.today().day]
+    )
+    
+    eat_month = forms.TypedChoiceField(
+        label="Month",
+        choices=MONTH_CHOICE,
+        coerce=int,
+        empty_value=MONTH_CHOICE[date.today().month]
+    )
+    
+    eat_hour = forms.TypedChoiceField(
+        label="Hour",
+        choices=HOUR_CHOICE,
+        coerce=int,
+        empty_value=HOUR_CHOICE[0],
+    )
+    
+    eat_minute = forms.TypedChoiceField(
+        label="Minute",
+        choices=MINUTE_CHOICE,
+        coerce=int,
+        empty_value=MINUTE_CHOICE[0],
+    )
     
     class Meta:
         model = Reservation
         field = "__all__"
-        exclude = ['reservation_id', 'review']
+        exclude = ['review', 'eat_date', 'eat_time']
         
-        # widgets = {
-        #     'customer_name' :
-        #     'phone_num' :
-        #     'vip_room' :
-        #     'table' : 
-        #     'eat_time' :
-        # }
+        widgets = {
+            'customer_name' : forms.TextInput(attrs={'class':'form-control'}),
+            'num_of_people' : forms.NumberInput(attrs={'class':'form-control'}),
+            'special_message' : forms.Textarea(attrs={'class':'form-control'}),
+        }
         
     def __init__(self, *args, **kwargs):
         super(ReservationForm, self).__init__(*args, **kwargs)
@@ -64,32 +76,11 @@ class ReservationTableForm(forms.ModelForm):
         ("6", "B3"),
     )
     
-    table = forms.MultipleChoiceField(label="Table Choice", choices=TABLES_CHOICES, required=False)
+    table_name = forms.MultipleChoiceField(label="Table Choice", choices=TABLES_CHOICES, required=False)
     
     class Meta:
         model = ReservationTable
         fields = ['table_name',]
-        exclude = []
-        
-    def __init__(self, *args, **kwargs):
-        super(ReservationTableForm, self).__init__(*args, **kwargs)
-
-class ReservationTableForm(forms.ModelForm):
-    TABLES_CHOICES = (
-        ("1", "A1"),
-        ("2", "A2"),
-        ("3", "A3"),
-        ("4", "B1"),
-        ("5", "B2"),
-        ("6", "B3"),
-    )
-    
-    table = forms.MultipleChoiceField(label="Table Choice", choices=TABLES_CHOICES, required=False)
-    
-    class Meta:
-        model = ReservationTable
-        fields = ()
-        exclude = []
         
     def __init__(self, *args, **kwargs):
         super(ReservationTableForm, self).__init__(*args, **kwargs)
@@ -100,12 +91,15 @@ class ReservationVipForm(forms.ModelForm):
         ("2", "VIP2"), 
     ) 
     
-    vip_room = forms.MultipleChoiceField(label="VIP Room", choices=VIP_CHOICES, required=False)
+    vip_name = forms.MultipleChoiceField(label="VIP Room", choices=VIP_CHOICES, required=False)
     
     class Meta:
         model = ReservationVip
-        fields = ()
-        exclude = []
+        fields = ('vip_name',)
+        
+        widgets = {
+            'vip_name' : forms.SelectMultiple(attrs={'class':'form-control'}),
+        }
         
     def __init__(self, *args, **kwargs):
         super(ReservationVipForm, self).__init__(*args, **kwargs)
